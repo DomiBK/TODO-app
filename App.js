c8d48871f089da98e37d056bda5a79a4a400b7d0
DomiBK <148908251+DomiBK@users.noreply.github.com>
2024-04-17 08:24:52 +0200

    Update App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import TaskList from './TaskList';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (text) => {
+    setTasks([...tasks, { id: Date.now(), text }]);
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  return (
+    <div className="app">
+      <h1>Task Manager</h1>
+      <TaskList tasks={tasks} removeTask={removeTask} />
+      <AddTaskForm addTask={addTask} />
+    </div>
+  );
+}
+
+export default App;
